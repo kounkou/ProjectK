09147e404399a2c4c03df0e09c614918bd995a1f
Jacques <gkounkou@gmail.com>
2017-09-09 23:02:11 +0200

    Update main.cpp
    Added a new comment style.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
-// <main.cpp> -*- C++ -*-
+// Copyright (c) 2017 godbod, All rights reserved. 
 
-// Copyright (C) 2017
-
-/** @file main
- *  This is a main entry file
- */
+////////////////////////////////////////////////////////////////////////////////
+// FILE Template.h
+//  
+// DESCRIPTION
+// This is the main file implementation 
+//
+//
+// DESIGN DOCUMENTS
+// none 
+//
+// DATE          BY                         Issue #      REMARKS
+// 09-Sept-2017  Godfrain Jacques KOUNKOU   N/A          Initial release
+//
+////////////////////////////////////////////////////////////////////////////////
 
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
@@ -12,6 +21,10 @@
 #include "FilterProxyModel.h"
 #include <QQmlContext>
 
+///
+/// @brief main file implementation
+/// @param [in] argc : number of parameters
+/// @param [in] argv : parameters given as char* []
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
